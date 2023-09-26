9cb8051e75254dde1a1a671302b89a61a14edf68
Roodini <roodinijoaquin@gmail.com>
2023-09-26 07:11:20 -0700

    Worked on adding event listeners for all 3 buttons Also replaced console.log messages with DOM methods but encountered a little bug along the way which displays the winner over and over again. WE ARE BACK FROM THE LONG ASS BREAK YOU GUYS, LIFE HAS NOT BEATEN ME YET |THEY ARE GONNA NEED A FUCKING ARMY TO GO UP AGAINST ME|

--- script.js
+++ script.js
@@ -1,61 +1,99 @@
 let playerScore = 0;
 let computerScore = 0;
 
-  const game = () => {
-  for (let round = 1; playerScore < 5 && computerScore < 5; round++) {
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const outcomeDiv = document.querySelector('.outcome');
+const p = document.createElement('p');
 
-  function getComputerChoice() {
-    let choicesAvailable = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = choicesAvailable[Math.floor(Math.random()*choicesAvailable.length)];
-    return randomChoice;
-  }
-    
-    function rounds(playerSelection, computerSelection){
-    if (playerSelection == "Rock" && computerSelection == "Scissors"){
+
+const computerPlay = () => {
+    const arrOfChoices = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * arrOfChoices.length);
+    const compChoice = arrOfChoices[randomNum];
+    return compChoice;
+}
+const playRound = (playerSelection, computerSelection) => {
+    console.log('1', playerSelection, '2', computerSelection);
+    if(playerSelection === computerSelection){
+        p.innerText = `You tied! You both picked ${playerSelection}`;
+        outcomeDiv.appendChild(p);
+    }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         playerScore++;
-       return "Rock beats scissors, you win.";
-    
-    }else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-        computerScore++;
-       return "Rock beats scissors, computer wins.";
-       
-    }else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+        p.innerText = "You won! Rock crushes scissors";
+        outcomeDiv.appendChild(p);
+    }else if(playerSelection === 'paper' && computerSelection === 'rock'){
         playerScore++;
-       return "Scissors beats paper, you win.";
-       
-    }else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        computerScore++;
-       return "Scissors beats paper, computer wins.";
-       
-    }else if (playerSelection == "Paper" && computerSelection == "Rock"){
+        p.innerText = "You won! Paper folds rock";
+        outcomeDiv.appendChild(p);
+    }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         playerScore++;
-       return "Paper beats rock, you win.";
-       
-    }else if (playerSelection == "Rock" && computerSelection == "Paper"){
+        p.innerText = "You won! Scissors cuts paper";
+        outcomeDiv.appendChild(p);
+    }else if(playerSelection === 'rock' && computerSelection === 'paper'){
         computerScore++;
-       return "Paper beats rock, computer wins.";
-        
-    }if (playerSelection === computerSelection){
-        return "Tie!";
-        
+        p.innerText = "You lost! Paper folds rock";
+        outcomeDiv.appendChild(p);
+    }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+        computerScore++;
+        p.innerText = "You lost! Scissors cuts paper";
+        outcomeDiv.appendChild(p);
+    }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+        computerScore++;
+        p.innerText = "You lost! Rock crushes scissors";
+        outcomeDiv.appendChild(p);
     }
 }
-    let player = prompt('pick', '');
-    playerConverted = player.charAt(0).toUpperCase() + player.slice(1).toLowerCase();
-    const playerSelection = playerConverted;
-    const computerSelection = getComputerChoice();
-    console.log(rounds(playerSelection, computerSelection));
 
-    console.log(`Player pick: ${playerSelection}`);
-    console.log(`Computer pick: ${computerSelection}`);
-    console.log(`Computer score: ${computerScore}, Player score: ${playerScore}`);
-    
+const checkForWinner = (playerScore, computerScore) => {
+    if(playerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.classList.add('player-won');
+        h2.innerText = `You won ${playerScore} to ${computerScore} great job beating the computer!`;
+        outcomeDiv.appendChild(h2);
+    }else if(computerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.classList.add('computer-won');
+        h2.innerText = `Computer won ${computerScoreScore} to ${playerScore} you lost, womp womp lil fella!`;
+        outcomeDiv.appendChild(h2);
+    }
 }
-  }
-    game();
 
+rockButton.addEventListener('click', () =>{
+    const computerSelection = computerPlay();
+    const playerSelection = 'rock';
+    playRound(playerSelection, computerSelection);
+    checkForWinner(playerScore,computerScore);
+})
+
+paperButton.addEventListener('click', () =>{
+    const computerSelection = computerPlay();
+    const playerSelection = 'paper';
+    playRound(playerSelection, computerSelection);
+    checkForWinner(playerScore,computerScore);
+})
+
+scissorsButton.addEventListener('click', () =>{
+    const computerSelection = computerPlay();
+    const playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection);
+    checkForWinner(playerScore,computerScore);
+})
 /*
-Make event listeners for each of the 3 buttons so that they get the correct playerSelection
-add div for displaying results
-Display running score, and the winner when someone gets 5 points 
-*/
+const game = () => {
+    for(let i = 0; i < 5; i++){
+        const playerSelection = prompt("Choose what to throw", "Rock, Paper, or Scissors").toLowerCase();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection,computerSelection));
+    }
+    if(playerScore > computerScore){
+        return 'You beat the computer! You are a genus!';
+    }else if(playerScore < computerScore){
+        return 'You got beat by the computer! You suck!';
+    }else{
+        return 'You tied with the computer! Not too shabby.'
+    }
+}
+
+console.log(game());*/
