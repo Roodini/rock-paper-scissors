aba27b7c7b379582bcaca2fa2bb7c39f6fd8f06c
Roodini <roodinijoaquin@gmail.com>
2023-09-27 05:44:16 -0700

    Fixed bug of printing game winner forever. Made changes so that the game stops at 5 rounds and it adds a button that restarts the game and clears previous text. LETS GO GUYS

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 const outcomeDiv = document.querySelector('.outcome');
-const p = document.createElement('p');
+const playerScoreSpan = document.querySelector('.player-score');
+const computerScoreSpan = document.querySelector('.computer-score');
+let gameOver = false;
+const maxScore = 5;
 
 
 const computerPlay = () => {
@@ -15,85 +18,106 @@ const computerPlay = () => {
     return compChoice;
 }
 const playRound = (playerSelection, computerSelection) => {
-    console.log('1', playerSelection, '2', computerSelection);
+    const p = document.createElement('p');
     if(playerSelection === computerSelection){
         p.innerText = `You tied! You both picked ${playerSelection}`;
-        outcomeDiv.appendChild(p);
+        
     }else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         playerScore++;
         p.innerText = "You won! Rock crushes scissors";
-        outcomeDiv.appendChild(p);
+        
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
         playerScore++;
         p.innerText = "You won! Paper folds rock";
-        outcomeDiv.appendChild(p);
+        
     }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         playerScore++;
         p.innerText = "You won! Scissors cuts paper";
-        outcomeDiv.appendChild(p);
+        
     }else if(playerSelection === 'rock' && computerSelection === 'paper'){
         computerScore++;
         p.innerText = "You lost! Paper folds rock";
-        outcomeDiv.appendChild(p);
+        
     }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
         computerScore++;
         p.innerText = "You lost! Scissors cuts paper";
-        outcomeDiv.appendChild(p);
+        
     }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
         computerScore++;
         p.innerText = "You lost! Rock crushes scissors";
-        outcomeDiv.appendChild(p);
+        
     }
+    outcomeDiv.appendChild(p);
+}
+const updateScores = (playerScore, computerScore) => {
+    playerScoreSpan.innerText = `Player score: ${playerScore}`;
+    computerScoreSpan.innerText = `Computer score: ${computerScore}`;
 }
 
 const checkForWinner = (playerScore, computerScore) => {
-    if(playerScore === 5){
+    if(playerScore === maxScore || computerScore === maxScore){
+        
+        gameOver = true;
         const h2 = document.createElement('h2');
+       
+        if(playerScore === maxScore){
         h2.classList.add('player-won');
         h2.innerText = `You won ${playerScore} to ${computerScore} great job beating the computer!`;
-        outcomeDiv.appendChild(h2);
-    }else if(computerScore === 5){
-        const h2 = document.createElement('h2');
+    }else if(computerScore === maxScore){
         h2.classList.add('computer-won');
-        h2.innerText = `Computer won ${computerScoreScore} to ${playerScore} you lost, womp womp lil fella!`;
-        outcomeDiv.appendChild(h2);
+        h2.innerText = `Computer won ${computerScore} to ${playerScore} you lost, womp womp lil fella!`;
     }
+        outcomeDiv.appendChild(h2);
+        const removeButton = document.createElement('button');
+        removeButton.innerText = "Restart";
+        removeButton.addEventListener('click', () => {
+            restart();
+        });
+        outcomeDiv.appendChild(removeButton);
+}
 }
 
+const restart = () => {
+    playerScore = 0;
+    computerScore = 0;
+    gameOver = false;
+
+    while(outcomeDiv.firstChild){
+        outcomeDiv.removeChild(outcomeDiv.firstChild);
+    }
+    updateScores(playerScore, computerScore);
+}
 rockButton.addEventListener('click', () =>{
+    if (!gameOver){
     const computerSelection = computerPlay();
     const playerSelection = 'rock';
+    
     playRound(playerSelection, computerSelection);
     checkForWinner(playerScore,computerScore);
+    updateScores(playerScore, computerScore);
+    }
 })
 
 paperButton.addEventListener('click', () =>{
+    if(!gameOver){
     const computerSelection = computerPlay();
     const playerSelection = 'paper';
+    
     playRound(playerSelection, computerSelection);
     checkForWinner(playerScore,computerScore);
+    updateScores(playerScore, computerScore);
+    }
 })
 
 scissorsButton.addEventListener('click', () =>{
+    if(!gameOver){
     const computerSelection = computerPlay();
     const playerSelection = 'scissors';
+    
     playRound(playerSelection, computerSelection);
     checkForWinner(playerScore,computerScore);
-})
-/*
-const game = () => {
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("Choose what to throw", "Rock, Paper, or Scissors").toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection,computerSelection));
-    }
-    if(playerScore > computerScore){
-        return 'You beat the computer! You are a genus!';
-    }else if(playerScore < computerScore){
-        return 'You got beat by the computer! You suck!';
-    }else{
-        return 'You tied with the computer! Not too shabby.'
-    }
+    updateScores(playerScore, computerScore);
 }
-
-console.log(game());*/
+})
+//we need to make it so that the player/computer score stops counting after 5
+//we need to make it so that it only prints the winner of the game once and restarts the game
